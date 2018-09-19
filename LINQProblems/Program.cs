c6724ff629866e2e3236c38f7ad5ee770e26c4ee
DavidRileySchultz <rileyschultz@gmail.com>
2018-09-19 11:02:09 -0500

    have first linq problem working

---
+++ LINQProblems/Program.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LINQ linq = new LINQ();
+            linq.FindWordsWithTH();
+            Console.ReadKey();
+        }
+    }
+}
